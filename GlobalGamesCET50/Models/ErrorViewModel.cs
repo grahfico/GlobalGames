30a63efa3341423de2d8c63c2aa69bcad55b8ddc
Bruno Campos <grahfico@gmail.com>
2021-01-07 17:14:04 +0000

    Add project files.

---
+++ GlobalGamesCET50/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlobalGamesCET50.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
